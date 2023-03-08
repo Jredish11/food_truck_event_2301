812ce3465fc1aa38ad3262b004b31565845d7826
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-08 10:07:48 -0700

    test: Add test for potential revenue to food truck spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe FoodTruck do
     @food_truck3 = FoodTruck.new("Palisade Peach Shack") 
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   describe "#initialize" do
